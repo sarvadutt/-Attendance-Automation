a70ffef91854861c1eb55ec7bd6b71619b5bd59d
Akavarapu Sarva Dutt <96296712+sarvadutt@users.noreply.github.com>
2025-05-23 16:56:29 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/pkg/errors"
+	"github.com/potsbo/jobcan/account"
+	"github.com/potsbo/jobcan/config"
+)
+
+func jobcanTouch(ctx context.Context, c config.Config) (string, error) {
+	if !c.Valid() {
+		return "", errors.Errorf("Not enough credential given")
+	}
+	mode := os.Getenv("JOBCAN_ADIT")
+	if len(mode) == 0 {
+		return "", errors.Errorf("env var JOBCAN_ADIT is empty")
+	}
+
+	a := account.New(c)
+	if err := a.Login(); err != nil {
+		return "", errors.Wrap(err, "failed to login")
+	}
+	if err := a.ExecAttendance(mode); err != nil {
+		return "", errors.Wrap(err, "failed to record")
+	}
+	return fmt.Sprintf("Your \"%s\" has successfully been recorded.", mode), nil
+}
+
+func main() {
+	lambda.Start(jobcanTouch)
+}
